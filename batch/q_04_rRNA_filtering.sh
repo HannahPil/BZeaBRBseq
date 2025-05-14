5928835ff21a7a1e11b0959eaf6f984b56fab41e
hsnnah <160691670+HannahPil@users.noreply.github.com>
2025-05-14 15:14:05 -0400

    Update q_04_rRNA_filtering.sh

--- batch/q_04_rRNA_filtering.sh
+++ batch/q_04_rRNA_filtering.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#BSUB -J 03_trimming
+#BSUB -J 04_rRNA
 #BSUB -q sara
-#BSUB -n 1
-#BSUB -o 03_trimming.%J.out
-#BSUB -e 03_trimming.%J.err
+#BSUB -n 10
+#BSUB -o 04_rRNA.%J.out
+#BSUB -e 04_rRNA.%J.err
 #BSUB -R "span[hosts=1]"
 #BSUB -W 12:00
 
