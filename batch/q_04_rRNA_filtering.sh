18120982c79e4caff4183eb8288f0106141bd875
hsnnah <160691670+HannahPil@users.noreply.github.com>
2025-05-14 15:12:26 -0400

    Create q_04_rRNA_filtering.sh

---
+++ batch/q_04_rRNA_filtering.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#BSUB -J 03_trimming
+#BSUB -q sara
+#BSUB -n 1
+#BSUB -o 03_trimming.%J.out
+#BSUB -e 03_trimming.%J.err
+#BSUB -R "span[hosts=1]"
+#BSUB -W 12:00
+
+../scripts/04_rRNA_filtering.sh
