7bdfd98344d01f085bd2e799b0c00dc8a6ddf037
hsnnah <160691670+HannahPil@users.noreply.github.com>
2025-05-15 14:04:08 -0400

    Update q_05_STAR_alignment_GEMMA.sh

--- batch/q_05_STAR_alignment_GEMMA.sh
+++ batch/q_05_STAR_alignment_GEMMA.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#BSUB -J 05_STAR
+#BSUB -q sara
+#BSUB -n 10
+#BSUB -o 05_STAR.%J.out
+#BSUB -e 05_STAR.%J.err
+#BSUB -R "span[hosts=1]"
+#BSUB -W 12:00
 
+../scripts/05_STAR_alignment.sh Zea_mays
