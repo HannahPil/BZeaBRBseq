9b87ed4e8d88d3fe7aad788d6f1aa910631720e2
hsnnah <160691670+HannahPil@users.noreply.github.com>
2025-05-20 09:36:42 -0400

    Update q_08_trimming_stats.sh

--- batch/q_08_trimming_stats.sh
+++ batch/q_08_trimming_stats.sh
@@ -1,1 +1,10 @@
-./08_trimming_stats.sh
+#!/bin/bash
+#BSUB -J 08_trimstats
+#BSUB -q sara
+#BSUB -n 1
+#BSUB -o 08_trimstats.%J.out
+#BSUB -e 08_trimstats.%J.err
+#BSUB -R "span[hosts=1]"
+#BSUB -W 12:00
+
+../scripts/08_trimming_stats.sh
